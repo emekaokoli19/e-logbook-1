a88f87a24553eccb7a9de4b1768d389b745cd643
Oluwagbemiga <samuelifeoluwa540@gmail.com>
2023-07-26 11:41:08 +0200

    add reactive forms for login and signup component

--- src/app/views/auth/auth.module.ts
+++ src/app/views/auth/auth.module.ts
@@ -2,11 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { AuthRouter, routingComponents } from './auth-routing.module';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { UpdatePasswordComponent } from './update-password/update-password.component';
 
 @NgModule({
   declarations: [
     /* from auth-routing.module.ts ===== */
-    routingComponents,
+    RegisterComponent,
+    LoginComponent,
+    ForgotPasswordComponent, 
+    UpdatePasswordComponent
   ],
   imports: [
     CommonModule,
